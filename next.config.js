fb637296036cd8b944126651a835c426fd18f682
VisionProces <lopezjr@outlook.com>
2026-08-25 14:12:12 -0600

    chore: clean repository - remove all files

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    optimizePackageImports: ['@radix-ui/react-*'],
-  },
-}
-
-module.exports = nextConfig
